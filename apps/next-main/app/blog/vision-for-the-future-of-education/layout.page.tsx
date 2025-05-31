862836e477792e8adeb9d2605ba881566afe0405
Marviel <lukebechtel4@gmail.com>
2025-05-31 10:55:53 -0700

    add opengraph

---
+++ apps/next-main/app/blog/vision-for-the-future-of-education/layout.page.tsx
@@ -0,0 +1,56 @@
+export async function generateMetadata() {
+    try {
+        return {
+            title: `A Vision for the Future of Education | Reasonote`,
+            description: `A Vision for the Future of Education`,
+            openGraph: {
+                title: `A Vision for the Future of Education`,
+                description: `A Vision for the Future of Education`,
+                images: [
+                    {
+                        url: `/static/images/vision-for-the-future-of-education-header.jpg`,
+                        width: 1200,
+                        height: 630,
+                        alt: `A Vision for the Future of Education`
+                    }
+                ],
+                type: 'profile',
+            },
+            twitter: {
+                card: 'summary_large_image',
+                title: `A Vision for the Future of Education`,
+                description: `A Vision for the Future of Education`,
+                images: [
+                    {
+                        url: `/static/images/vision-for-the-future-of-education-header.jpg`,
+                        width: 1200,
+                        height: 630,
+                        alt: `A Vision for the Future of Education`
+                    }
+                ]
+            }
+        };
+    } catch (error) {
+        return {
+            title: 'Reasonote',
+            description: 'Learn anything with Reasonote',
+            openGraph: {
+                title: 'Reasonote',
+                description: 'Learn anything with Reasonote',
+                images: [
+                    {
+                        url: `/static/images/vision-for-the-future-of-education-header.jpg`, // Will return fallback image
+                        width: 1200,
+                        height: 630,
+                        alt: 'Reasonote'
+                    }
+                ],
+                type: 'website'
+            }
+        };
+    }
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return children;
+} 
